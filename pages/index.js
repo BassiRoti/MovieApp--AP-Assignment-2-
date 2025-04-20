6ac4a223242964bc1947305cf687bb92480a34cc
Abbas Rizvi <abbasriz1234567890@gmail.com>
2025-04-21 01:36:13 +0500

    part 1 completed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import fs from 'fs/promises';
+import path, { parse } from 'path';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,9 +14,59 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props.data)
+  const [data,setdata]=useState([]);
+  useEffect(()=>{
+    const temp=props.data.filter(val=>val.rating>=8.5);
+    setdata(temp);
+  },[props.data])
+  const r=useRouter();
+
+  const handleclick=()=>{
+    r.push('/Genre')
+  }
+
   return (
    <>
+   <ul>
+    {data.map(val=>(
+      <div key={val.id}>
+      <li>{val.title}</li>
+      <li>{val.releaseYear}</li>
+      <li>{val.rating}</li>
+      </div>      
+    ))}
+   </ul>
+
+   <button  onClick={()=>handleclick()}>Browse Genres</button>
    </>
   );
 }
+
+export async function getStaticProps() {
+  const p = path.join(process.cwd(), "Data", "Movies.json");
+  const data = await fs.readFile(p);
+  const parsed_data = JSON.parse(data);
+
+  // Access the movies array from parsed data
+  const movies = parsed_data.movies;
+
+  // Use map to transform the movies array
+  const filtered_data = movies.map(movie => ({
+    id: movie.id,
+    title: movie.title,
+    directorId: movie.directorId,
+    description: movie.description,
+    releaseYear: movie.releaseYear,
+    genreId: movie.genreId,
+    rating: movie.rating,
+  }));
+
+  return {
+    props: {
+      data: filtered_data,
+    },
+  };
+}
+
